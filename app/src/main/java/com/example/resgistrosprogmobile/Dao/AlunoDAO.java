51981e22ca0c113a1378efae7f1e1b020adc8d41
Breno Vergilio <bjvergilio47@gmail.com>
2021-05-13 10:33:04 -0400

    AlunoDAO e TrabDatabase completados

--- app/src/main/java/com/example/resgistrosprogmobile/Dao/AlunoDAO.java
+++ app/src/main/java/com/example/resgistrosprogmobile/Dao/AlunoDAO.java
@@ -1,4 +1,47 @@
 package com.example.resgistrosprogmobile.Dao;
 
-public class AlunoDAO {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.resgistrosprogmobile.Entities.AlunoEnt;
+
+import java.util.List;
+
+@Dao
+public interface AlunoDAO {
+
+    @Query("SELECT * FROM AlunoEnt WHERE Alunoid = :id LIMIT 1")
+    AlunoEnt get(int id);
+
+    @Query("SELECT * FROM AlunoEnt")
+    List<AlunoEnt> getAll();
+
+    @Query("SELECT * FROM AlunoEnt WHERE Alunoid IN (:alunoentId)")
+    List<AlunoEnt> loadAllByIds(int[] alunoentId);
+
+    @Query("SELECT * FROM AlunoEnt WHERE nomeAluno LIKE :name LIMIT 1")
+    AlunoEnt findByName(String name);
+
+    @Query("UPDATE AlunoEnt SET cursoId =:cursoID WHERE Alunoid == :alunoentId")
+    void updateCurso(int cursoID, int alunoentId);
+
+    @Query("UPDATE AlunoEnt SET nomeAluno =:name WHERE Alunoid == :alunoentId")
+    void updateName(String name, int alunoentId);
+
+    @Query("UPDATE AlunoEnt SET cpf =:cpf WHERE Alunoid == :alunoentId")
+    void updateCpf(String cpf, int alunoentId);
+
+    @Query("UPDATE AlunoEnt SET email =:email WHERE Alunoid == :alunoentId")
+    void updateEmail(String email, int alunoentId);
+
+    @Query("UPDATE AlunoEnt SET telefone =:telefone WHERE Alunoid == :alunoentId")
+    void updateTelefone(String telefone, int alunoentId);
+
+    @Insert
+    void insertAll(AlunoEnt... aluno);
+
+    @Delete
+    void delete(AlunoEnt aluno);
 }
--- app/src/main/java/com/example/resgistrosprogmobile/Database/TrabDatabase.java
+++ app/src/main/java/com/example/resgistrosprogmobile/Database/TrabDatabase.java
@@ -1,11 +1,28 @@
 package com.example.resgistrosprogmobile.Database;
 
+import android.content.Context;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.resgistrosprogmobile.Entities.AlunoEnt;
-import com.example.resgistrosprogmobile.Entities.CursoEnt;
+import com.example.resgistrosprogmobile.Dao.AlunoDAO;
+import com.example.resgistrosprogmobile.Dao.CursoDAO;
 
 @Database(entities = {AlunoEnt.class, CursoEnt.class},version = 1)
 public abstract class TrabDatabase extends RoomDatabase {
+
+    private static TrabDatabase INSTANCE;
+
+    public static TrabDatabase getDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder
+                    (context.getApplicationContext(),
+                     TrabDatabase.class,
+                     "Controle de Cursos").allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
+
+    public abstract AlunoDAO alunoModel();
+    public abstract CursoDAO cursoModel();
 }
